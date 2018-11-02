LineageOS/android_kernel_oneplus_sdm845

drivers: input: Import goodix and fingerprint_detect driver
Change-Id: Ia51e27d67f49b41e368af3727fc0a01f7e8b7fb3

drivers/input/fingerprint/fingerprint_detect/fingerprint_detect.h
@@ -1,0 +1,17 @@
+#ifndef __FINGERPRINT_DETETC_H_
+#define __FINGERPRINT_DETETC_H_
+
+struct fingerprint_detect_data {
+	struct device *dev;
+	int id0_gpio;
+	int id1_gpio;
+	int id2_gpio;
+	struct pinctrl         *fp_pinctrl;
+	struct pinctrl_state   *id_en_init;
+	struct pinctrl_state   *id_state_init;
+	int sensor_version;
+	int project_version;
+};
+extern int fp_version;
+#endif
+

GZR-Kernels/kernel_oneplus_sdm845

drivers/input/fingerprint/fingerprint_detect/fingerprint_detect.h
@@ -1,0 +1,18 @@
+#ifndef __FINGERPRINT_DETETC_H_
+#define __FINGERPRINT_DETETC_H_
+
+struct fingerprint_detect_data {
+	struct device *dev;
+	int id0_gpio;
+	int id1_gpio;
+	int id2_gpio;
+	struct pinctrl         *fp_pinctrl;
+	struct pinctrl_state   *id_en_init;
+	struct pinctrl_state   *id_state_up;
+	struct pinctrl_state   *id_state_down;
+	int sensor_version;
+	int project_version;
+};
+extern int fp_version;
+#endif
+